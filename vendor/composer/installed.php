c0ccae4711f1a666cdc5a69544b65abf1f47de88
JuanCarlosCatalino <juancarloscatalino534@gmail.com>
2025-04-24 11:57:49 -0500

    ala

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'yucra/sigi',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '242f510e81b42d6d8c6d7de5da5df446ab77cb94',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.9.3',
+            'version' => '6.9.3.0',
+            'reference' => '2f5c94fe7493efc213f643c23b1b1c249d40f47e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'yucra/sigi' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '242f510e81b42d6d8c6d7de5da5df446ab77cb94',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
